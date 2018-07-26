31c44559c504b9322770fe4ae3fad3a1cf5b5e29
Barry <5648875+BarryMode@users.noreply.github.com>
2018-07-25 22:49:25 -0500

    v1.0.0-beta.1 - GUI and Taxonomy

--- blackhole.php
+++ blackhole.php
@@ -1,8 +1,8 @@
 <?php
 namespace Grav\Plugin;
 
-use Grav\Common\Plugin;
 use Grav\Common\Grav;
+use Grav\Common\Plugin;
 use Grav\Common\Page\Page;
 
 class BlackholePlugin extends Plugin {
@@ -15,50 +15,32 @@ class BlackholePlugin extends Plugin {
   }
 
   public function onPageInitialized() {
-    if (!defined('GRAV_URL')) {
-      define('GRAV_URL', $this->grav['uri']->base() . $this->grav['pages']->baseUrl());
-    }
+    if (!defined('ROOT_URL')) define('ROOT_URL', $this->grav['uri']->rootUrl(true));
 
-    if (!empty($_GET['pages']) && $_GET['pages'] == 'all') {
-      ob_start();
-      // get all routes from grav
-      $routes = $this->grav['pages']->routes();
-      // unset modular pages
-      foreach ($routes as $path => $dir) {
-        if (strpos($path, '/_') !== false) {
-          unset($routes[$path]);
-        }
-      }
-      $this->content = json_encode($routes, JSON_UNESCAPED_SLASHES);
-
-    } else if (!empty($_GET['generate']) && $_GET['generate'] == 'true') {
-      // get generate_command from plugin settings
+    if (!empty($_GET['blackhole']) && $_GET['blackhole'] === 'generate') {
       $output_url   = $this->config->get('plugins.blackhole.generate.output_url');
       $output_path  = $this->config->get('plugins.blackhole.generate.output_path');
       $routes       = $this->config->get('plugins.blackhole.generate.routes');
       $simultaneous = $this->config->get('plugins.blackhole.generate.simultaneous');
       $assets       = $this->config->get('plugins.blackhole.generate.assets');
+      $taxonomy     = $this->config->get('plugins.blackhole.generate.taxonomy');
       $force        = $this->config->get('plugins.blackhole.generate.force');
       $this->content =
-        'bin/plugin blackhole generate ' . GRAV_URL .
+        'bin/plugin blackhole generate ' . ROOT_URL .
         ($output_url   ? ' --output-url '   . $output_url   : '') .
         ($output_path  ? ' --output-path '  . $output_path  : '') .
         ($routes       ? ' --routes '       . $routes       : '') .
         ($simultaneous ? ' --simultaneous ' . $simultaneous : '') .
         ($assets       ? ' --assets'                        : '') .
+        ($taxonomy     ? ' --taxonomy'                      : '') .
         ($force        ? ' --force'                         : '')
       ;
     }
   }
 
   public function onOutputRendered() {
-    // publish page routes
-    if (!empty($_GET['pages']) && $_GET['pages'] == 'all') {
-      ob_end_clean();
-      echo $this->content;
-    }
     // action for generate button
-    if (!empty($_GET['generate']) && $_GET['generate'] == 'true') {
+    if (!empty($_GET['blackhole']) && $_GET['blackhole'] === 'generate') {
       shell_exec($this->content);
     }
   }
